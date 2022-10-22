18eb2908ffc9957e1aefc2ca62e2d15d49edb1f7
spear97 <72108425+spear97@users.noreply.github.com>
2022-10-22 17:47:11 -0500

    Unreal Engine Project Creation
    Created an Unreal Engine 5 Project for this repository

---
+++ ActionGame/Intermediate/Build/Win64/ActionGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.33.31630
+#include "C:/Users/seanp/Documents/Unreal Projects/ActionGame/Intermediate/Build/Win64/ActionGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ ActionGame/Intermediate/Build/Win64/UnrealEditor/Inc/ActionGame/ActionGameGameMode.generated.h
@@ -0,0 +1,90 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ACTIONGAME_ActionGameGameMode_generated_h
+#error "ActionGameGameMode.generated.h already included, missing '#pragma once' in ActionGameGameMode.h"
+#endif
+#define ACTIONGAME_ActionGameGameMode_generated_h
+
+#define FID_ActionGame_Source_ActionGame_ActionGameGameMode_h_12_SPARSE_DATA
+#define FID_ActionGame_Source_ActionGame_ActionGameGameMode_h_12_RPC_WRAPPERS
+#define FID_ActionGame_Source_ActionGame_ActionGameGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_ActionGame_Source_ActionGame_ActionGameGameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAActionGameGameMode(); \
+	friend struct Z_Construct_UClass_AActionGameGameMode_Statics; \
+public: \
+	DECLARE_CLASS(AActionGameGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/ActionGame"), ACTIONGAME_API) \
+	DECLARE_SERIALIZER(AActionGameGameMode)
+
+
+#define FID_ActionGame_Source_ActionGame_ActionGameGameMode_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAActionGameGameMode(); \
+	friend struct Z_Construct_UClass_AActionGameGameMode_Statics; \
+public: \
+	DECLARE_CLASS(AActionGameGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/ActionGame"), ACTIONGAME_API) \
+	DECLARE_SERIALIZER(AActionGameGameMode)
+
+
+#define FID_ActionGame_Source_ActionGame_ActionGameGameMode_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	ACTIONGAME_API AActionGameGameMode(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AActionGameGameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(ACTIONGAME_API, AActionGameGameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AActionGameGameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	ACTIONGAME_API AActionGameGameMode(AActionGameGameMode&&); \
+	ACTIONGAME_API AActionGameGameMode(const AActionGameGameMode&); \
+public:
+
+
+#define FID_ActionGame_Source_ActionGame_ActionGameGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	ACTIONGAME_API AActionGameGameMode(AActionGameGameMode&&); \
+	ACTIONGAME_API AActionGameGameMode(const AActionGameGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(ACTIONGAME_API, AActionGameGameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AActionGameGameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AActionGameGameMode)
+
+
+#define FID_ActionGame_Source_ActionGame_ActionGameGameMode_h_9_PROLOG
+#define FID_ActionGame_Source_ActionGame_ActionGameGameMode_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_ActionGame_Source_ActionGame_ActionGameGameMode_h_12_SPARSE_DATA \
+	FID_ActionGame_Source_ActionGame_ActionGameGameMode_h_12_RPC_WRAPPERS \
+	FID_ActionGame_Source_ActionGame_ActionGameGameMode_h_12_INCLASS \
+	FID_ActionGame_Source_ActionGame_ActionGameGameMode_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_ActionGame_Source_ActionGame_ActionGameGameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_ActionGame_Source_ActionGame_ActionGameGameMode_h_12_SPARSE_DATA \
+	FID_ActionGame_Source_ActionGame_ActionGameGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_ActionGame_Source_ActionGame_ActionGameGameMode_h_12_INCLASS_NO_PURE_DECLS \
+	FID_ActionGame_Source_ActionGame_ActionGameGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ACTIONGAME_API UClass* StaticClass<class AActionGameGameMode>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_ActionGame_Source_ActionGame_ActionGameGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
